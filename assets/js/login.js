e42b5a8f77dbd7dd099801e4dfc194a9e99a662d
callmetarush <tarush.nagpal7@gmail.com>
2017-12-02 19:03:33 +0530

    changes

---
+++ assets/js/login.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    $("#login").click(function () {
+
+                
+    $("#message").html('');
+                 var user = new Object();
+
+                 user.regno = $('#regno').val();
+                 user.password = $('#password').val();
+
+                 $.ajax({
+
+                     url: 'https://ccs.csivit.com/authenticate',
+
+                     type: 'POST',
+
+                     dataType: 'json',
+
+                     data: user,
+
+                     success: function (data, textStatus, xhr) {
+
+                        console.log(data);
+                        window.location = "https://ccs.csivit.com/dashboard";
+                        
+                     },
+
+                     error: function (xhr, textStatus, errorThrown) {
+
+                         console.log('Error in Operation');
+                          $("#message").html('Incorrect password');
+                      }
+        });
+    });
+});
+
+ var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
