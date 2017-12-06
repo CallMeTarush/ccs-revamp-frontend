0641c9958ff0df0c4830e375e5da2388c205f10c
akigugale <gugaleaki@gmail.com>
2017-12-07 00:15:42 +0530

    changeS

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,19 +2,19 @@ $(document).ready(function () {
   console.log(data);
   if(data.design) {
      document.getElementById("design").style.cursor = "not-allowed";
-     $("#question_img").attr("src","assets/images/design-bw.png");
+     $("#design-img").attr("src","assets/images/design-bw.png");
     }
   if(data.technical) {
     document.getElementById("technical").style.cursor = "not-allowed";
-    $("#question_img").attr("src","assets/images/technicalbw.png");
+    $("#technical-img").attr("src","assets/images/technicalbw.png");
   }
   if(data.management) {
     document.getElementById("management").style.cursor = "not-allowed";
-    $("#question_img").attr("src","assets/images/managementbw.png"); 
+    $("#managment-img").attr("src","assets/images/managementbw.png"); 
    }
   if(data.advtechnical) {
     document.getElementById("advtechnical").style.cursor = "not-allowed";
-    $("#question_img").attr("src","assets/images/advtech-bw.png");
+    $("#advtech-img").attr("src","assets/images/advtech-bw.png");
   }
 });
 function on() {
