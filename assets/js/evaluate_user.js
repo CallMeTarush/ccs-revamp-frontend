bf533b5ae2a1496e13ccec1fc5fd1dd704ccc030
callmetarush <tarush.nagpal7@gmail.com>
2017-12-08 12:12:06 +0530

    changes Fri Dec  8 12:12:06 IST 2017

--- assets/js/evaluate_user.js
+++ assets/js/evaluate_user.js
@@ -8,7 +8,30 @@ function displayUsers() {
 
     for (i = 0; i < users.length; i++) {
         if (users[i].isEvaluated) {
-          document.getElementById("user").innerHTML += "<tr><td>" + users[i].regno + "</td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
+          
+          if (window.location == "https://ccs.csivit.com/usertests/technical") {
+
+                alert("Is Evaluated!");
+                document.getElementById("user").innerHTML += "<tr><td>" + i "</td><td><a href='/evaluate/" + users[i].regno + "/technical'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
+
+          }
+          else if (window.location == "https://ccs.csivit.com/usertests/management") {
+              
+              alert("Is Evaluated!");
+              document.getElementById("user").innerHTML += "<tr><td><a href='/evaluate/" + users[i].regno + "/management'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
+
+          } else if (window.location == "https://ccs.csivit.com/usertests/design") {
+              
+              alert("Is Evaluated!");
+              document.getElementById("user").innerHTML += "<tr><td><a href='/evaluate/" + users[i].regno + "/design'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
+
+          } else if (window.location == "https://ccs.csivit.com/usertests/advtechnical") {
+              
+              alert("Is Evaluated!");
+              document.getElementById("user").innerHTML += "<tr><td><a href='/evaluate/" + users[i].regno + "/advtechnical'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
+
+          }
+
         }
         else {
           if (window.location == "https://ccs.csivit.com/usertests/technical") {
