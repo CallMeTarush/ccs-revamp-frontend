87f726c9eb76756d7a01f7c8df436ae76b7db3d8
callmetarush <tarush.nagpal7@gmail.com>
2017-12-08 12:16:47 +0530

    changes Fri Dec  8 12:16:47 IST 2017

--- assets/js/evaluate_user.js
+++ assets/js/evaluate_user.js
@@ -7,32 +7,7 @@ $(document).ready(function() {
 function displayUsers() {
 
     for (i = 0; i < users.length; i++) {
-        if (users[i].isEvaluated) {
-          
-          if (window.location == "https://ccs.csivit.com/usertests/technical") {
-
-              alert("Is Evaluated!");
-              document.getElementById("user").innerHTML += "<tr><td>" + i + "</td><td><a href='/evaluate/" + users[i].regno + "/technical'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
-
-          }
-          else if (window.location == "https://ccs.csivit.com/usertests/management") {
-              
-              alert("Is Evaluated!");
-              document.getElementById("user").innerHTML += "<tr><td>" + i + "</td><td><a href='/evaluate/" + users[i].regno + "/management'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
-
-          } else if (window.location == "https://ccs.csivit.com/usertests/design") {
-              
-              alert("Is Evaluated!");
-              document.getElementById("user").innerHTML += "<tr><td>" + i + "</td><td><a href='/evaluate/" + users[i].regno + "/design'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
-
-          } else if (window.location == "https://ccs.csivit.com/usertests/advtechnical") {
-              
-              alert("Is Evaluated!");
-              document.getElementById("user").innerHTML += "<tr><td>" + i + "</td><td><a href='/evaluate/" + users[i].regno + "/advtechnical'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
-
-          }
-
-        }
+        
         else {
           if (window.location == "https://ccs.csivit.com/usertests/technical") {
                 document.getElementById("user").innerHTML += "<tr><td>" + i + "</td><td><a href='/evaluate/" + users[i].regno + "/technical'>" + users[i].regno + "</a></td><td>" + users[i].name + "</td><td>" + users[i].phone + "</td><td>" + users[i].isEvaluated + "</td></tr>";
