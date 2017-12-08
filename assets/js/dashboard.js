fa0cae1f1984b480712d3d3bc5f46998054cd79b
callmetarush <tarush.nagpal7@gmail.com>
2017-12-08 16:46:35 +0530

    changes Fri Dec  8 16:46:35 IST 2017

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -61,7 +61,7 @@ function managementClick() {
 };
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 10, 2017 23:59:59").getTime();
+var countDownDate = new Date("Dec 9, 2017 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -73,11 +73,13 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  
+
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+  hours += days*24;
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = hours + " hr <span class='lap-show'>:</span><br class='br-mob'> " + minutes + " min <span class='lap-show'>:</span><br class='br-mob'> " + seconds + " sec ";
  // days + " days <span class='lap-show'>:</span><br class='br-mob'> " +
